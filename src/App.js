f9fd30c07014e91f06aa55c0ab6dc5dd3b3c6d54
Timothée Nguyen <tnguyen@normandiewebschool.fr>
2021-04-29 21:05:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+//import { useState } from "react";
 //import InputBar from "./InputBar";
 //import ToDoList from "./ToDoList/ToDoList";
 //import DisplayStopwatch from './Stopwatch/DisplayStopwatch';
